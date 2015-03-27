3e7d0c5eb0f2a71d06c40653f372688d3b03e5fa
Patrick HENG <patrick.heng@hetic.net>
2015-03-27 18:04:42 +0100

    Angular - Starting searching files in folder at drop

--- app/public/js/directives/droppableDirective.js
+++ app/public/js/directives/droppableDirective.js
@@ -28,16 +28,55 @@ daw.directive('droppable', function() {
 
 			el.ondrop = function(e) {
 				e.preventDefault();
-				for(var i = 0; i < e.dataTransfer.files.length; i++) {
-					var file = e.dataTransfer.files[i].path;
-					console.log(path.dirname(file));
-					console.log('file dropped');
+
+				var data = e.dataTransfer;
+				var items = data.items;
+
+				for (var i=0; i<items.length; i++) {
+					// webkitGetAsEntry is where the magic happens
+					var entry = {
+						path : data.files[i].path,
+						name : data.files[i].name,
+						item : data.items[i].webkitGetAsEntry(),
+						directory : path.dirname(data.files[i].path)
+					}
+
+					if (entry.item) {
+						traverseFileTree(entry);
+					}
 				}
 
 				this.className = "";
 				this.innerHTML = "Drop your files here";
 				return false;
 			}
+
+
+			// :::: Searching files in folders
+			function traverseFileTree(entry, path) {
+				path = path || "";
+
+				var item = entry.item;
+
+				console.log(entry);
+				console.log(item.isFile);
+
+				if (item.isFile) {
+					item.file(function(file) {
+						console.log("File: " +  entry.path);
+						console.log("Directory: " +  entry.directory);
+					});
+				} else if (item.isDirectory) {
+					// Get folder contents
+					var dirReader = item.createReader();
+					dirReader.readEntries(function(entries) {
+						for (var i=0; i<entries.length; i++) {
+							traverseFileTree(entries[i], path + item.name + "/");
+						}
+					});
+				}
+			}
+
 		}
 	}
 });
