e1e2071938a74f26211b3d8f58152c14c5d6835e
Mathieu <mathieu.letyrant@gmail.com>
2015-03-31 16:30:56 +0200

    Angular - Directive for informationPage
    - When user click on link Twitter/Portfolio open browser.

---
+++ app/public/js/directives/openDirective.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// NPM Required
+var open = require('open');
+
+daw.directive('open', function() {
+
+	return {
+		restrict: 'A',
+		link: function($scope, element, attr) {
+
+			/*
+			 * Add Cursor Pointer
+			 */
+			element.css('cursor', 'pointer');
+
+			/*
+			 * When user click on it, check value and open navigator
+			 */
+			element.bind('click', function(event) {
+
+				var value = this.innerHTML;
+
+				if(value.indexOf('@') === 0) {
+					open('https://twitter.com/'+value);
+				} else {
+					open('http://'+value);
+				}
+
+			});
+		}
+	}
+
+});
