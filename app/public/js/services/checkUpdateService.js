2170ffae2c91abfa872eaa93bccdfe4f13de062d
Mathieu <mathieu.letyrant@gmail.com>
2015-04-01 14:14:19 +0200

    Angular - CheckNewVersion done

--- app/public/js/services/checkUpdateService.js
+++ app/public/js/services/checkUpdateService.js
@@ -6,12 +6,66 @@ var updater = require('node-webkit-updater');
 var upd 	= new updater(pkg);
 var copyPath, execPath;
 
-daw.service('checkUpdateService', function() {
+daw.service('checkUpdateService', function($rootScope) {
 
 	var that = this;
 
+	$rootScope.newVersionAvailable = false;
+
+	/*
+	 * Check if new Version
+	 */
 	that.launch = function() {
-		// TODO Do stuff
+
+		if(gui.App.argv.length) {
+
+			copyPath = gui.App.argv[0];
+			execPath = gui.App.argv[1];
+
+			upd.install(copyPath, function(err) {
+				if(!err) {
+					upd.run(execPath, null);
+					gui.App.quit();
+				}
+			});
+
+		}
+		else {
+
+			upd.checkNewVersion(function(error, newVersionExists, manifest) {
+				if (!error && newVersionExists) {
+					$rootScope.newVersionAvailable = true;
+				}
+			});
+
+		}
+	};
+
+	/*
+	 * Update the app
+	 */
+	that.update = function() {
+		upd.checkNewVersion(function(error, newVersionExists, manifest) {
+			if (!error && newVersionExists && $rootScope.newVersionAvailable) {
+				download(manifest);
+			}
+		});
+	};
+
+	/*
+	 * Download new app
+	 */
+	var download = function(manifest) {
+		upd.download(function(error, filename) {
+			if (!error) {
+				upd.unpack(filename, function(error, newAppPath) {
+					if (!error) {
+						upd.runInstaller(newAppPath, [upd.getAppPath(), upd.getAppExec()],{});
+						gui.App.quit();
+					}
+				}, manifest);
+			}
+		}, manifest);
 	};
 
 });
