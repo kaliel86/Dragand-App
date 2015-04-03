72f81a9f0287b9ef8a1e9972769cffd597f1abb9
Mathieu <mathieu.letyrant@gmail.com>
2015-04-03 16:28:20 +0200

    Angular - First step for limit Characters in list

--- app/public/js/services/guiService.js
+++ app/public/js/services/guiService.js
@@ -1,6 +1,6 @@
 'use strict';
 
-daw.service('guiService', function() {
+daw.service('guiService', function($rootScope) {
 
 	var that = this;
 
@@ -13,6 +13,20 @@ daw.service('guiService', function() {
 	that.init = function() {
 		nativeMenuBar.createMacBuiltin("Dragand");
 		win.menu = nativeMenuBar;
+		that.listenResize();
+	};
+
+	/*
+	 * Listen Resize and add new Width in RootScope
+	 *
+	 * Width : 320px -> 17 characters
+	 *
+	 * So for exemple Width : 400px -> 21.25 -> 21 characters
+	 */
+	that.listenResize = function() {
+		window.addEventListener("resize", function() {
+			$rootScope.width = win.width;
+		});
 	};
 
 });
