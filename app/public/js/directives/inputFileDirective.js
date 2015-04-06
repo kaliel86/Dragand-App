59b4015580ef45a10b92a806b4fe9d4112187ea3
Patrick HENG <patrick.heng@hetic.net>
2015-04-06 23:47:00 +0200

    Front - InputFileDirective add subtitlesV2Service

--- app/public/js/directives/inputFileDirective.js
+++ app/public/js/directives/inputFileDirective.js
@@ -1,6 +1,6 @@
 'use strict';
-
-daw.directive('file', function() {
+var pathNode = require('path');
+daw.directive('file', function(subtitlesV2Service, $rootScope) {
 	return {
 		restrict: 'E',
 		scope	: {
@@ -10,28 +10,41 @@ daw.directive('file', function() {
 		template: "<span id='textTypeFile'>{{text}}</span>" +
 		"<input id='inputTypeFile' type='file' {{multiple}}>",
 
-		link: function($scope, element, attr) {
+		link: function($scope, element, attr, controller) {
 
 			var textTypeFile  = document.querySelector('#textTypeFile');
 			var inputTypeFile = document.querySelector('#inputTypeFile');
 			var evObj 		  = document.createEvent('MouseEvents');
+			
 
 			/*
 			 * Trigger click on input type File
 			 */
 			textTypeFile.addEventListener("click", function() {
-
 				evObj.initEvent('click', true, false);
-
 				inputTypeFile.dispatchEvent(evObj);
-
 			});
 
-			/*
+			/* 
 			 * Paths to files selected
 			 */
 			inputTypeFile.addEventListener("change", function() {
-				console.log(this.value);
+				// Check if the format of the file is good
+				if($scope.$parent.checkExtension(this.value)) {
+					var path, name, dir = '';
+					path = this.value;
+
+					dir = this.value.split(pathNode.sep);
+					name = dir[dir.length-1];
+					dir.pop();
+					dir = dir.join(pathNode.sep);
+
+					subtitlesV2Service.get(name, path, dir, $rootScope.list.length).then(function() {
+						$scope.$parent.count++;
+					});
+
+				}
+
 			}, false);
 
 		}
