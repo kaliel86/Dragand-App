beeaf7fe1eab17ec47ff502b61ce0b9c3fa99e62
Mathieu <mathieu.letyrant@gmail.com>
2015-04-14 17:08:06 +0200

    Gulp - Cosmetic

--- gulpfile.js
+++ gulpfile.js
@@ -75,10 +75,10 @@ gulp.task('cleanBuildFolder', function() {
  gulp.task('build', ['cleanBuildFolder'], function() {
 
  	// Read package.json
-    var package = require('./package.json')
+    var package = require('./package.json');
 
     // Find out which modules to include
-    var modules = []
+    var modules = [];
     if (!!package.dependencies) {
         modules = Object.keys(package.dependencies)
                 .filter(function(m) { return m != 'nodewebkit' })
@@ -86,21 +86,21 @@ gulp.task('cleanBuildFolder', function() {
     }
 
  	// Which platforms should we build
-    var platforms = []
-    if (process.argv.indexOf('--win') > -1)     platforms.push('win')
-    if (process.argv.indexOf('--mac') > -1)     platforms.push('osx')
-    if (process.argv.indexOf('--linux32') > -1) platforms.push('linux32')
-    if (process.argv.indexOf('--linux64') > -1) platforms.push('linux64')
+    var platforms = [];
+    if (process.argv.indexOf('--win') > -1)     platforms.push('win');
+    if (process.argv.indexOf('--mac') > -1)     platforms.push('osx');
+    if (process.argv.indexOf('--linux32') > -1) platforms.push('linux32');
+    if (process.argv.indexOf('--linux64') > -1) platforms.push('linux64');
 
     // Build for All platforms
-    if (process.argv.indexOf('--all') > -1) platforms = [ 'win', 'osx', 'linux32', 'linux64' ]
+    if (process.argv.indexOf('--all') > -1) platforms = [ 'win', 'osx', 'linux32', 'linux64' ];
 
     // If no platform where specified, determine current platform
     if (!platforms.length) { 
-        if      (process.platform === 'darwin') platforms.push('osx')
-        else if (process.platform === 'win32')  platforms.push('win')
-        else if (process.arch === 'ia32')       platforms.push('linux32')
-        else if (process.arch === 'x64')        platforms.push('linux64')
+        if      (process.platform === 'darwin') platforms.push('osx');
+        else if (process.platform === 'win32')  platforms.push('win');
+        else if (process.arch === 'ia32')       platforms.push('linux32');
+        else if (process.arch === 'x64')        platforms.push('linux64');
     }
 
  	var nw = new NwBuilder({
